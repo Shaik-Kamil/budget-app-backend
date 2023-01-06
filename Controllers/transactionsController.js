592afa669fd7096fac09a05cb98b8286bb8becfb
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2023-01-06 14:39:51 -0500

    fixed error in code

--- Controllers/transactionsController.js
+++ Controllers/transactionsController.js
@@ -5,7 +5,7 @@ const { validateURL } = require('../models/validation');
 
 //! list of all transactions
 transaction.get('/', (req, res) => {
-  res.status(200).json({ transactionArray });
+  res.status(200).json(transactionArray);
 });
 
 //! transaction at index number
@@ -24,12 +24,10 @@ transaction.post('/', validateURL, (req, res) => {
 });
 
 //! update a transaction
-transaction.put('/:index', (req, res) => {
-  const { index } = req.params.index;
-
-  if (transactionArray[index]) {
+transaction.put('/:index', validateURL, (req, res) => {
+  if (transactionArray[req.params.index]) {
     transactionArray[req.params.index] = req.body;
-    res.status(200).json(transactionArray[index]);
+    res.status(200).json(transactionArray[req.params.index]);
   } else {
     res.status(404).json({ error: 'Transaction not found' });
   }
