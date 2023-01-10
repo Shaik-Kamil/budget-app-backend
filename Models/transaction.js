91f130d2a349e44093fcfb1fbcc69280be0e499d
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2023-01-10 16:18:26 -0500

    added more data in models

--- Models/transaction.js
+++ Models/transaction.js
@@ -13,4 +13,25 @@ module.exports = [
     from: 'Chase Bank',
     category: 'Expenses',
   },
+  {
+    item_name: 'Car insurance',
+    amount: -600,
+    date: '01/23/2023',
+    from: 'Chase Bank',
+    category: 'Expenses',
+  },
+  {
+    item_name: 'Income',
+    amount: 1000,
+    date: '01/23/2023',
+    from: 'Job',
+    category: 'Income',
+  },
+  {
+    item_name: 'Netflix',
+    amount: 17.99,
+    date: '01/15/2023',
+    from: 'Netflix',
+    category: 'Entertainment',
+  },
 ];
